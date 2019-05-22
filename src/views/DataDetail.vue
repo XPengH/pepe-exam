01313cb617e06f6c763c1b759fce224e65b68d72
pepe <xphssw@gmail.com>
2019-05-22 23:11:52 +0800

    [修改] 样式更新

--- src/views/DataDetail.vue
+++ src/views/DataDetail.vue
@@ -1,26 +1,54 @@
 <template>
   <div class="dataDetail">
+    <el-button size="small" type="primary" style="margin: 0 0 20px 10px" @click="backPage">返回上一页</el-button>
     <el-container :class="[isMobile?'container':'pcContainer']">
-      <div style="width: 100%;">
-        <p>
-          商品名:&nbsp;{{detail.name}}
-        </p>
-        <p>
-          类型:&nbsp;{{detail.type}}
-        </p>
-        <p>
-          价格:&nbsp;{{detail.price}}(元)
-        </p>
-        <p>
-          数量:&nbsp;{{detail.number}}(件)
-        </p>
-        <p>
-          生产厂商:&nbsp;{{detail.company}}生产厂商:&nbsp;
-        </p>
-        <p>
-          厂商地址:&nbsp;{{detail.address}}
-        </p>
-      </div>
+      <el-table
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column type="expand">
+          <template slot-scope="props">
+            <el-form label-position="left" inline class="table-expand">
+              <el-form-item label="商品名称：">
+                <span>{{ props.row.name }}</span>
+              </el-form-item>
+              <el-form-item label="生产厂商：">
+                <span>{{ props.row.company }}</span>
+              </el-form-item>
+              <el-form-item label="商品 ID：">
+                <span>{{ props.row.id }}</span>
+              </el-form-item>
+              <el-form-item label="商品分类：">
+                <span>{{ props.row.type }}</span>
+              </el-form-item>
+              <el-form-item label="厂商地址：">
+                <span>{{ props.row.address }}</span>
+              </el-form-item>
+              <el-form-item label="商品价格：">
+                <span>{{ props.row.price }}元</span>
+              </el-form-item>
+              <el-form-item label="商品数量：">
+                <span>{{ props.row.number }}件</span>
+              </el-form-item>
+            </el-form>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="商品名称"
+          prop="name">
+        </el-table-column>
+        <el-table-column
+          label="类型"
+          prop="type">
+        </el-table-column>
+        <el-table-column
+          label="价格"
+          prop="price">
+        </el-table-column>
+        <el-table-column
+          label="数量"
+          prop="number">
+        </el-table-column>
+      </el-table>
       <div id="echarts" :class="[isMobile?'echartMobileStyle':'echartStyle']"></div>
     </el-container>
   </div>
@@ -33,10 +61,16 @@ export default {
   name: 'DataDetail',
   data() {
     return {
-      detail: {}
+      detail: {},
+      tableData: []
     };
   },
   props: ['isMobile'],
+  methods: {
+    backPage() {
+      this.$router.go(-1)
+    }
+  },
   mounted() {
     if (this.$route.params.cid!==undefined) {
       // 基于准备好的dom，初始化echarts实例
@@ -46,6 +80,7 @@ export default {
     }
     if (this.$route.query.detail) {
       this.detail = JSON.parse(this.$route.query.detail)
+      this.tableData = [this.detail]
     }
   }
 }
@@ -53,6 +88,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.table-expand {
+  font-size: 0;
+}
+.table-expand label {
+  width: 90px;
+  color: #99a9bf;
+}
+.table-expand .el-form-item {
+  margin-right: 0;
+  margin-bottom: 0;
+  width: 50%;
+}
 h3{
   font-size: 15px;
   margin: 10px 0;
